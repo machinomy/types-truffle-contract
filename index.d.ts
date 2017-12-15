6550457819c288e2275fbe34511590b310be353f
Sergey Ukustov <sergey@ukstv.me>
2017-12-15 15:56:54 +0300

    Add address field, fix styling

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,36 @@
 declare module 'truffle-contract' {
-    import Web3 = require('web3')
+  import Web3 = require('web3')
 
-    namespace contract {
-        export interface TruffleContract<A> {
-            'new' (opts?: Web3.TxData): Promise<A>
-            at(address: string): Promise<A>
-            deployed (): Promise<A>
-
-            setProvider (provider: Web3.Provider): void
-            setNetwork (networkId: string | number): void
-            resetAddress (): void
-
-            hasNetwork (networkId: string | number): boolean
-            isDeployed (): boolean
-        }
-
-        export interface TransactionEvent {
-            event: string
-            args: any
-        }
-
-        export interface TransactionResult {
-            tx: string
-            logs: Array<TransactionEvent>
-            receipt: Web3.TransactionReceipt
-        }
+  namespace contract {
+    export interface DeployedContract {
+      address: string
     }
 
-    function contract<A>(json: any): contract.TruffleContract<A>
-    export = contract
+    export interface TruffleContract<A> {
+      'new' (opts?: Web3.TxData): Promise<A & DeployedContract>
+      at (address: string): Promise<A & DeployedContract>
+      deployed (): Promise<A & DeployedContract>
+
+      setProvider (provider: Web3.Provider): void
+      setNetwork (networkId: string | number): void
+      resetAddress (): void
+
+      hasNetwork (networkId: string | number): boolean
+      isDeployed (): boolean
+    }
+
+    export interface TransactionEvent {
+      event: string
+      args: any
+    }
+
+    export interface TransactionResult {
+      tx: string
+      logs: Array<TransactionEvent>
+      receipt: Web3.TransactionReceipt
+    }
+  }
+
+  function contract <A>(json: any): contract.TruffleContract<A>
+  export = contract
 }
